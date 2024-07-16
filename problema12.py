60991be5da90c325a59134bd0ac0d9a0102d6cbc
DaniSelmi <selmiralopezespiritu@gmail.com>
2024-07-16 02:21:50 +0000

    cargsa

--- problema12.py
+++ problema12.py
@@ -6,11 +6,26 @@
 #.txt -- text/plain
 #.zip -- application/zip
 
-diccionario = {".gif":"image/gif",
-               ".jpg":"image/jpg",
-               ".jpeg":"image/jpeg",
-               ".png":"image/png",
-               ".pdf":"application/pdf",
-               ".txt":"text/plain",
-               ".zip":"application/zip"
+diccionario = {"gif":"image/gif",
+               "jpg":"image/jpg",
+               "jpeg":"image/jpeg",
+               "png":"image/png",
+               "pdf":"application/pdf",
+               "txt":"text/plain",
+               "zip":"application/zip"
                }
+
+print(diccionario)
+
+respuesta = input("Nombre del archivo: ")
+respuesta = respuesta.lower().strip()
+
+nombre, sufijo = respuesta.split('.')
+
+if sufijo.lower() in diccionario.keys:
+    x = diccionario.get(sufijo)
+    print(x)
+else:
+    print("application/octet-steam")
+
+
